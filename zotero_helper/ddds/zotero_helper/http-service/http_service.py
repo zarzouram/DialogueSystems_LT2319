5f6115eb432c302174022ac64160e1675804af1b
zarzouram <muhammadzarzoura@gmail.com>
2021-10-27 16:59:29 +0200

    Tests OK. general count, count per type, and count items/per type under a folder

--- zotero_helper/ddds/zotero_helper/http-service/http_service.py
+++ zotero_helper/ddds/zotero_helper/http-service/http_service.py
@@ -172,14 +172,18 @@ def action_success_response():
    }
    """
     )
-    data2print = []
-    status = request.get_json()["request"]["parameters"]
-    for s in status:
-        data2print.append({"request": {s: [status[s]["grammar_entry"], status[s]["value"]]}})
-    status = request.get_json()["context"]["facts"]
-    for s in status:
-        data2print.append({"facts": {s: [status[s]["grammar_entry"], status[s]["value"]]}})
-    print_data(["action_success_response", *data2print])
+    # data2print = []
+    # status = request.get_json()["request"]["parameters"]
+    # for s in status:
+    #     data2print.append({f"request_{s}_grammar_entry": status[s]["grammar_entry"]})
+    #     data2print.append({f"request_{s}value         ": status[s]["value"]})
+
+    # status = request.get_json()["context"]["facts"]
+    # for s in status:
+    #     data2print.append({f"facts_{s}_grammar_entry  ": status[s]["grammar_entry"]})
+    #     data2print.append({f"facts_{s}value           ": status[s]["value"]})
+    # print_data(["action_success_response", *data2print])
+
     payload = response_template.render()
     response = app.response_class(response=payload, status=200, mimetype='application/json')
     logger.info("Sending successful action response to TDM", response=response)
@@ -192,52 +196,93 @@ def action_success_response():
 def folder_id():
     payload = request.get_json()
     name = payload["request"]["parameters"]["folder_name"]
+    folder_list, folder_name = None, None
     if name:
-        folder_name = name["value"].lower()
+        folder_name = name["grammar_entry"]
         folder_list = zot.all_collections()
         if folder_list:
             for folder in folder_list:
-                if folder["data"]["name"].lower() == folder_name:
+                if folder["data"]["name"] == folder_name:
                     id_folder = folder["key"]  # folder found
                     break
             else:
-                id_folder = "0"
+                id_folder = "noid"
         else:
-            id_folder = "0 "  # no folders in the lib
+            id_folder = "noid"  # no folders in the lib
+        # id_folder = ["noid", "id_found"][0]
     else:
-        id_folder = "2"  # user did not specify folder
+        id_folder = 2  # user did not specify folder
 
-    if id_folder == "0":
+    if id_folder == "noid":
         SCORE_BOARD["folder"]["status"] = "notfound"
         out_score_board(SCORE_BOARD)
 
-    print_data(["folder_name", name, id_folder, SCORE_BOARD])
+    id_folder = str(id_folder)
+    print_data(["folder_name", folder_list, name, id_folder, folder_name, SCORE_BOARD])
     return query_response(value=id_folder, grammar_entry=id_folder)
+
+
+def count_records(r_type="record", folder_id=None):
+    if r_type == "record":
+        if folder_id is None:
+            count = zot.count_items()
+        else:
+            count = zot.collection(folder_id)["meta"]["numItems"]
+    else:
+        if folder_id is None:
+            items = zot.everything(zot.items())
+        else:
+            items = zot.everything(zot.collection_items(folder_id))
+
+        count = 0
+        for item in items:
+            count += (item["data"]["itemType"] == r_type)
+
+    print("kkkkkkkkkkk", r_type, folder_id, count)
+    return count
 
 
 # return count + type
 @app.route("/items_count_type", methods=['POST'])
 def items_num_type():
     payload = request.get_json()
-    item_type = payload["request"]["parameters"]["type"]
-    # folder = payload["request"]["parameters"]["folder"]
+    req_type = payload["request"]["parameters"]["type"]
+    req_folder = payload["request"]["parameters"]["folder_id"]["value"]
 
-    if not item_type:
-        num = zot.count_items()
-        r_type = "records" if num > 1 else "record"
-    else:
-        num = 0
-        items = zot.everything(zot.items())
-        r_type = " ".join(re.findall("[A-Z][^A-Z]*|[a-z]*", item_type["value"]))
-        r_type = r_type.strip().lower()
-        for item in items:
-            num += (item['data']['itemType'] == item_type["value"])
-        if num > 1:
-            r_type += "s"
+    item_type = req_type["value"] if req_type else "record"
+    folder = req_folder if req_folder != "2" else None
 
-    num = str(num) + " " + r_type
-    print_data(["count_type", item_type, num])
-    return query_response(value=None, grammar_entry=num)
+    count_num = count_records(item_type, folder)
+    count_unit = item_type
+    if item_type != "record":
+        count_unit = " ".join(re.findall("[A-Z][^A-Z]*|[a-z]*", item_type)).strip().lower()
+
+    if count_num > 1:
+        count_unit += "s"
+
+    count_num_unit = str(count_num) + " " + count_unit
+    print_data(["count_type", count_num, count_unit, payload["request"]["parameters"]["folder_id"]])
+    return query_response(value="num", grammar_entry=count_num_unit)
+
+
+# return count only
+@app.route("/items_count_only", methods=['POST'])
+def items_num():
+    payload = request.get_json()
+    req_type = payload["request"]["parameters"]["type"]
+    req_folder = payload["request"]["parameters"]["folder_id"]["value"]
+
+    item_type = req_type["value"] if req_type else "record"
+    folder = req_folder if req_folder != "2" else None
+
+    count_num = count_records(item_type, folder)
+
+    # When to return grammar_entry and when not! This should not be drived by
+    # programming requerement! To interact with if/then statement the value
+    # should have the number, but if I return None in grammar_entry I got an
+    # buildig error.
+    # print_data(["count_only", item_type, num])
+    return query_response(value=str(count_num), grammar_entry=str(count_num))
 
 
 @app.route("/check_item_type", methods=['POST'])
@@ -246,7 +291,7 @@ def check_type():
     item_type = payload["request"]["parameters"]["type"]
 
     if not item_type:
-        r_type = "records"
+        r_type = "record"
     else:
         r_type = " ".join(re.findall("[A-Z][^A-Z]*|[a-z]*", item_type["value"]))
         # r_type_g = item_type["grammar_entry"]
@@ -254,29 +299,7 @@ def check_type():
     print_data(["check_item_type", item_type, r_type])
     # When I set the value the response to ReportEmpty when I have a question
     # How many journal article do I have is empty
-    return query_response(value=None, grammar_entry=r_type)
-
-
-# return count only
-@app.route("/items_count_only", methods=['POST'])
-def items_num():
-    payload = request.get_json()
-    item_type = payload["request"]["parameters"]["type"]
-
-    if not item_type:
-        num = zot.count_items()
-    else:
-        num = 0
-        items = zot.everything(zot.items())
-        for item in items:
-            num += (item['data']['itemType'] == item_type["value"])
-
-    # When to return grammar_entry and when not! This should not be drived by
-    # programming requerement! To interact with if/then statement the value
-    # should have the number, but if I return None in grammar_entry I got an
-    # buildig error.
-    print_data(["count_only", item_type, num])
-    return query_response(value=str(num), grammar_entry=str(num))
+    return query_response(value="None", grammar_entry=r_type)
 
 
 @app.route("/folder_fail_response", methods=['POST'])
@@ -306,8 +329,18 @@ def folder_fail_response():
     return response
 
 
+@app.route("/mydebug", methods=['POST'])
+def debug():
+    # payload = request.get_json()
+    # item_type = payload["request"]["parameters"]["folder_id"]
+
+    # print_data(["debug", item_type])
+    return query_response(value=None, grammar_entry="d")
+
+
 def print_data(data):
-    print("\n\n......................")
+    print("\n\n......................\n")
     for d in data:
-        pprint(f"--> {d}")
+        pprint(f"{d}")
+        print()
     print("......................\n\n")
